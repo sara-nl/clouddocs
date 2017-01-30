524a712a0b1f5c82175662c78bc4e11977dcca91
Ander <aal.aiesec@gmail.com>
2017-01-30 13:53:38 +0100

    Create cloudinit_fix.sh

---
+++ UvA-20170201/code/cloudinit_fix.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo su -
+
+cat <<EOF > /etc/init.d/clear-cloud-init.sh
+#!/bin/bash
+truncate -s 0 "/etc/udev/rules.d/70-persistent-net.rules"
+rm -rf "/var/lib/cloud"
+EOF
+
+chmod +x /etc/init.d/clear-cloud-init.sh
+update-rc.d clear-cloud-init.sh defaults
