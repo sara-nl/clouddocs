92c3ad4607e311ff8d35d1854ea6dd8982a5314a
Nuno Ferreira <nunolf@gmail.com>
2017-06-10 13:49:26 +0200

    Update find_and_replace.sh

--- bootcamp-20170615/find_and_replace.sh
+++ bootcamp-20170615/find_and_replace.sh
@@ -1,5 +1,5 @@
 find . -name "*.markdown" -exec sed -i \
--e 's/\[TUDelft logo\](\/images\/TUDelft_logo.png)/\[TUEindhoven logo\](\/images\/TUEindhoven_logo.png)/' \
+-e 's/\[TUDelft logo\](\/images\/TUDelft_logo.png)/\[TUEindhoven logo\](\/images\/TUEindhoven_logo.png)/g' \
 -e 's/2016\-11\-10/2017\-06\-15/g' \
 -e 's/126\: local_images_Cursus/147\: Courses_img/g' \
 {} \;
