54a3fb0ed2433575dc44333a15898654833703a0
Ander <aal.aiesec@gmail.com>
2019-04-02 16:21:02 +0200

    File to configure ubuntu 18's kernel

---
+++ assets/ubuntu18gpukernelpin.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# from: https://gist.github.com/ITJesse/380489a742c92261a1e02935dafec240
+
+KERNEL_VERSION="4.4.0-142-generic"
+
+sudo apt-get update
+sudo aptitude install -y linux-image-${KERNEL_VERSION}-generic \
+     linux-headers-${KERNEL_VERSION} linux-image-extra-${KERNEL_VERSION}-generic
+     
+sudo sed -i "s/GRUB_DEFAULT=.*/GRUB_DEFAULT=\"Advanced options for Ubuntu>Ubuntu, with Linux ${KERNEL_VERSION}-generic\"/" /etc/default/grub
+sudo update-grub
+sudo reboot
