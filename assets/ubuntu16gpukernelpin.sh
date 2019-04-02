4e027b6d8b73afb9a341fb979d0e91599e9f2697
Ander <aal.aiesec@gmail.com>
2019-04-02 16:23:38 +0200

    Update ubuntu16gpukernelpin.sh

--- assets/ubuntu16gpukernelpin.sh
+++ assets/ubuntu16gpukernelpin.sh
@@ -5,7 +5,7 @@
 KERNEL_VERSION="4.4.0-142-generic"
 
 sudo apt-get update
-sudo aptitude install -y linux-image-${KERNEL_VERSION}-generic \
+sudo apt install -y linux-image-${KERNEL_VERSION}-generic \
      linux-headers-${KERNEL_VERSION} linux-image-extra-${KERNEL_VERSION}-generic
      
 sudo sed -i "s/GRUB_DEFAULT=.*/GRUB_DEFAULT=\"Advanced options for Ubuntu>Ubuntu, with Linux ${KERNEL_VERSION}-generic\"/" /etc/default/grub
